581522a8d0dcdb8a98c991d0b30d17806999aabc
Taoshif1 <161155604+Taoshif1@users.noreply.github.com>
2025-04-15 00:23:12 +0600

    Update script.js

--- Create Save Feature/script.js
+++ Create Save Feature/script.js
@@ -10,9 +10,9 @@ function increment() {
 
 function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    let countStr = " " + count + "  -  "
+    let countStr = count + " - "
     // 3. Render the variable in the saveEl using innerText
-    saveEl.innerText += countStr
+    saveEl.textContent += countStr
     // NB: Make sure to not delete the existing content of the paragraph
     console.log(count)
 }
